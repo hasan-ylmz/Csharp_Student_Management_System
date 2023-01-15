b9570d1faf7ee35d7b19580620c63c3f36ddb368
Hasan <77204260+hasan-ylmz@users.noreply.github.com>
2023-01-15 17:04:09 +0300

    Update WebForm5.aspx.cs

--- proje/WebForm5.aspx.cs
+++ proje/WebForm5.aspx.cs
@@ -14,7 +14,7 @@ namespace proje
         {
 
         }
-        SqlConnection bagla = new SqlConnection("Data Source=LAPTOP-FJ35BN93;Initial Catalog=ödev;Integrated Security=True");
+        SqlConnection bagla = new SqlConnection("Data Source=LAPTOP-(.....);Initial Catalog=ödev;Integrated Security=True");
         protected void Button1_Click(object sender, EventArgs e)
         {
 
